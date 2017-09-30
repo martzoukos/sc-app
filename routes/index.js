713538fc102d126e99052a9f942cc5536aaca7ce
Spiros Martzoukos <s.d.martzoukos@gmail.com>
2017-09-30 23:04:39 +0200

    Add first proof of concept for connection of modules.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,51 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+	const fractal = require('sc-components');
+	fractal.load().then(() => {
+		let blocks = '';
+
+		// Block text
+	  fractal.components.render('@block-text', {
+	    text: 'Hello there'
+	  }).then(html => {
+	  	blocks = blocks + html;
+	  });
+		// Block image
+	  fractal.components.render('@block-image', {
+	    alt: 'Hello there',
+	    src: 'https://images.contentful.com/fo9twyrwpveg/5nCI6r6Z68yoq2WocomYoS/99f9985edfd1073b3936435835d6e5a1/_DSC0082.jpg'
+	  }).then(html => {
+	  	blocks = blocks + html;
+	  });
+		// Block quote
+	  fractal.components.render('@block-quote', {
+	    quote: 'A quote',
+	    cite: 'Someone'
+	  }).then(html => {
+	  	blocks = blocks + html;
+	  });
+		// Block video
+	  fractal.components.render('@block-video', {
+	    src: 'https://www.youtube.com/embed/JrcH-4wHK9w'
+	  }).then(html => {
+	  	blocks = blocks + html;
+	  });
+		// Block link
+	  fractal.components.render('@block-link', {
+	    id: '21345'
+	  }).then(html => {
+	  	blocks = blocks + html;
+	  });
+
+	  // I know, I know...
+	  setTimeout(() => {
+	  	res.render('index', { title: 'Structured Content app', html: blocks });
+	  }, 500);
+	});
+
 });
+
 
 module.exports = router;
